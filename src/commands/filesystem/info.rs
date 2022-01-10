19b0e6145a7bd663edc72335abd49d4816b7c973
Matt Fellenz <matt@felle.nz>
2022-01-10 09:50:11 -0800

    Commands: implement filesystem::info

--- src/commands/filesystem/info.rs
+++ src/commands/filesystem/info.rs
@@ -1,4 +1,6 @@
 use crate::commands::Runnable;
+use crate::device::{filesystem as fs, send};
+use std::str::FromStr;
 
 #[derive(clap::Parser)]
 pub struct Args {
@@ -8,6 +10,38 @@ pub struct Args {
 
 impl Runnable for Args {
 	fn run(self, dev: crate::presence::Presence<crate::device::Device>) {
-		todo!();
+		let mut dev = dev.expect_one();
+		if let Some(file) = self.file {
+			// print info for the file
+			let send_data = send::FileMetadataByName::new(fs::Category::default(), fs::FileName::from_str(&file).unwrap());
+			let metadata = dev.get_file_metadata_by_name(&send_data).unwrap();
+			println!("Size: {}", metadata.size);
+			println!("Address: {:0>#8x}", metadata.addr);
+			println!("File type: {}", metadata.file_type);
+			println!("Last modified: {}", metadata.timestamp);
+			println!("Version: {:0>#8x}", metadata.version);
+			let is_link = metadata.linked_category != fs::Category::None;
+			println!("Is link? {}", is_link);
+			if is_link {
+				println!("Linked category: {}", metadata.linked_category);
+				println!("Linked filename: {}", metadata.linked_name);
+			}
+		} else {
+			// list files
+			let files = dev.list_all_files(fs::Category::default()).unwrap();
+			println!("Num files: {}", files.len());
+			println!("Address    Mtime                       Version    Size  Type  Name\n");
+			for metadata in files {
+				println!(
+					"{address:0>#8x}  {mtime}  {version:0>#8x}  {size: >4}  {ty: <4}  {name}",
+					size = metadata.size,
+					address = metadata.addr,
+					ty = metadata.file_type,
+					mtime = metadata.timestamp,
+					version = metadata.version,
+					name = metadata.name
+				);
+			}
+		}
 	}
 }
