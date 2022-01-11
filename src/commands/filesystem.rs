8415d0cf5a5756e819f79bf155d068a280f632e2
Matt Fellenz <matt@felle.nz>
2022-01-11 13:23:20 -0800

    Remove filesystem::{cp, mv} commands
    Both of these commands are not necessary and can be synthesized.
    
    - cp: `cli filesystem cat source.file | cli filesystem sponge dest.file`
    - mv: cp and then `cli filesystem rm source.file`

--- src/commands/filesystem.rs
+++ src/commands/filesystem.rs
@@ -1,12 +1,10 @@
 use crate::commands::Runnable;
 
 mod cat;
-mod cp;
 mod edit;
 mod info;
 #[cfg(target_os = "linux")]
 mod mount;
-mod mv;
 mod rm;
 mod sponge;
 
@@ -28,8 +26,6 @@ enum Commands {
 	/// Output the contents of a file.
 	/// To "pull" a file from the device, you can add ` > localfile` to the command line.
 	Cat(cat::Args),
-	/// Copy file.
-	Cp(cp::Args),
 	/// Edit file using $EDITOR.
 	Edit(edit::Args),
 	/// List files with no argument; print file metadata with an argument.
@@ -37,8 +33,6 @@ enum Commands {
 	#[cfg(target_os = "linux")]
 	/// Mount the device as a FUSE filesystem.
 	Mount(mount::Args),
-	/// Move a file.
-	Mv(mv::Args),
 	/// Write stdin to a remote file.
 	/// To "push" a file to the device, you can add ` < localfile` to the command line.
 	Sponge(sponge::Args),
@@ -50,11 +44,9 @@ impl Runnable for Commands {
 	fn run(self, dev: crate::presence::Presence<crate::device::Device>) {
 		match self {
 			Commands::Cat(args) => args.run(dev),
-			Commands::Cp(args) => args.run(dev),
 			Commands::Edit(args) => args.run(dev),
 			Commands::Info(args) => args.run(dev),
 			Commands::Mount(args) => args.run(dev),
-			Commands::Mv(args) => args.run(dev),
 			Commands::Rm(args) => args.run(dev),
 			Commands::Sponge(args) => args.run(dev),
 		}
