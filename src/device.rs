e434bf5d979afd4e4841272b234f294562c20b9b
Matt Fellenz <matt@felle.nz>
2022-01-11 09:29:53 -0800

    Split ext payload code into header+footer
    This way, if another function wants to do something weird with the actual data, it can.

--- src/device.rs
+++ src/device.rs
@@ -1,5 +1,6 @@
 use crate::crc::CRCSerialPort;
 use core::any::type_name;
+use encde::{Decode, Encode};
 use log::{debug, trace, warn};
 use std::io::{Read, Write};
 use std::path::Path;
@@ -256,15 +257,9 @@ impl Device {
 		Ok(ret)
 	}
 	fn rx_ext_payload_length(&mut self) -> Result<usize> {
-		debug!("rx extended payload length");
 		let ret = self.rx_vex_varint()?;
-		match ret {
-			4.. => Ok(ret - 4),
-			received_length => Err(DeviceError::Protocol(ProtocolError::BadLength {
-				entity: "extended payload length",
-				received_length,
-			})),
-		}
+		debug!("rx extended payload length");
+		Ok(ret)
 	}
 	fn rx_expect<T: encde::Decode + encde::Encode + PartialEq + std::fmt::Debug>(&mut self, entity: &'static str, expected: &T) -> Result<()> {
 		let received = self.rx::<T>()?;
@@ -310,31 +305,50 @@ impl Device {
 		self.rx_echoed_command(command_id)?;
 		self.rx_simple_payload()
 	}
-	fn begin_ext_command(&mut self, command_id: CommandId, data: &[u8]) -> Result<()> {
-		debug!("begin extended command {:#02x} with {} bytes of data", command_id, data.len());
+	fn tx_ext_command_header(&mut self, command_id: CommandId, payload_len: usize) -> Result<()> {
+		debug!("begin extended command {:#02x} with {} bytes of data", command_id, payload_len);
 		self.port.begin_tx_crc();
 		self.tx_command_header()?;
 		self.tx(&Self::EXT_COMMAND)?;
 		self.tx(&command_id)?;
-		self.tx_vex_varint(data.len())?;
-		self.tx_raw_data(data)?;
-		self.port.end_tx_crc()?;
+		self.tx_vex_varint(payload_len)?;
 		Ok(())
 	}
-	fn end_ext_command<T: encde::Decode>(&mut self, sent_command: CommandId) -> Result<T> {
-		debug!("end extended command {:#02x}", sent_command);
+	fn tx_ext_command_footer(&mut self) -> Result<()> {
+		self.port.end_tx_crc().map_err(DeviceError::from)
+	}
+	fn begin_ext_command(&mut self, command_id: CommandId, data: &[u8]) -> Result<()> {
+		self.tx_ext_command_header(command_id, data.len())?;
+		self.tx_raw_data(data)?;
+		self.tx_ext_command_footer()?;
+		Ok(())
+	}
+	fn rx_ext_command_header(&mut self, sent_command: CommandId) -> Result<usize> {
+		trace!("rx extended command header");
 		self.port.begin_rx_crc();
 		self.rx_response_header()?;
 		self.rx_expect("echoed command", &Self::EXT_COMMAND)?;
-		let payload_len = self.rx_ext_payload_length()?;
+		// subtract echoed command byte, plus 16-bit CRC in rx_ext_command_footer
+		let payload_len = self.rx_ext_payload_length()? - 3;
 		self.rx_expect("echoed actual command", &sent_command)?;
+		Ok(payload_len)
+	}
+	fn rx_ext_command_footer(&mut self) -> Result<()> {
+		trace!("rx extended command footer");
+		if self.port.end_rx_crc()? {
+			Ok(())
+		} else {
+			Err(DeviceError::Protocol(ProtocolError::InvalidCrc))
+		}
+	}
+	fn end_ext_command<T: encde::Decode>(&mut self, sent_command: CommandId) -> Result<T> {
+		debug!("end extended command {:#02x}", sent_command);
+		// subtract response byte
+		let payload_len = self.rx_ext_command_header(sent_command)? - 1;
 		self.rx_response_byte()?;
 		let raw_payload = self.rx_bytes(payload_len)?;
-		if !self.port.end_rx_crc()? {
-			Err(DeviceError::Protocol(ProtocolError::InvalidCrc))
-		} else {
-			Self::decode_from_data(&raw_payload)
-		}
+		self.rx_ext_command_footer()?;
+		Self::decode_from_data(&raw_payload)
 	}
 }
 
