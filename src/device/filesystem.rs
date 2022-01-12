3965a21b6127ead68d14670fb4a7950149460251
Matt Fellenz <matt@felle.nz>
2022-01-12 08:39:59 -0800

    Replace macro with const generic

--- src/device/filesystem.rs
+++ src/device/filesystem.rs
@@ -135,76 +135,75 @@ pub type FileSize = u32;
 pub type FileIndex = u8;
 pub type PacketSize = u16;
 
-macro_rules! fixed_string_type {
-	($ident:ident, $size:tt) => {
-		#[derive(Encode, Decode, Default, Clone, Copy, Eq)]
-		#[repr(transparent)]
-		pub struct $ident([u8; $size]);
-		impl std::str::FromStr for $ident {
-			type Err = ();
-			fn from_str(s: &str) -> Result<Self, Self::Err> {
-				if s.len() > $size {
-					return Err(());
-				}
-				let mut ret: Self = Self::default();
-				for (idx, byte) in s.bytes().chain(std::iter::repeat(0)).take($size).enumerate() {
-					ret.0[idx] = byte;
-				}
-				Ok(ret)
+#[derive(Encode, Decode, Clone, Copy, Eq)]
+#[repr(transparent)]
+pub struct FixedString<const N: usize>([u8; N]);
+impl<const N: usize> Default for FixedString<N> {
+	fn default() -> Self {
+		Self([0u8; N])
+	}
+}
+impl<const N: usize> std::str::FromStr for FixedString<N> {
+	type Err = ();
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		if s.len() > N {
+			return Err(());
+		}
+		let mut ret: Self = Self::default();
+		for (idx, byte) in s.bytes().chain(std::iter::repeat(0)).take(N).enumerate() {
+			ret.0[idx] = byte;
+		}
+		Ok(ret)
+	}
+}
+impl<const N: usize> std::convert::TryFrom<&str> for FixedString<N> {
+	type Error = <Self as std::str::FromStr>::Err;
+	fn try_from(s: &str) -> Result<Self, Self::Error> {
+		<Self as std::str::FromStr>::from_str(s)
+	}
+}
+impl<const N: usize> FixedString<N> {
+	pub fn as_str(&self) -> Result<&str, std::str::Utf8Error> {
+		let mut len = N;
+		for (idx, &byte) in self.0.iter().enumerate() {
+			if byte == 0 {
+				len = idx;
+				break;
 			}
 		}
-		impl std::convert::TryFrom<&str> for $ident {
-			type Error = <Self as std::str::FromStr>::Err;
-			fn try_from(s: &str) -> Result<Self, Self::Error> {
-				<Self as std::str::FromStr>::from_str(s)
+		std::str::from_utf8(&self.0[..len])
+	}
+}
+impl<const N: usize> std::fmt::Debug for FixedString<N> {
+	fn fmt(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+		write!(formatter, "FixedString<{}> ", N)?;
+		self.as_str().map_err(|_| std::fmt::Error::default())?.fmt(formatter)
+	}
+}
+impl<const N: usize> std::fmt::Display for FixedString<N> {
+	fn fmt(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+		self.as_str().map_err(|_| std::fmt::Error::default())?.fmt(formatter)
+	}
+}
+impl<const N: usize> PartialEq for FixedString<N> {
+	fn eq(&self, other: &Self) -> bool {
+		for (&c1, &c2) in self.0.iter().zip(other.0.iter()) {
+			if c1 != c2 {
+				return false;
+			}
+			// end of string: quit early
+			// c1 == c2 due to previous block
+			if c1 == 0 {
+				return true;
 			}
 		}
-		impl $ident {
-			pub fn as_str(&self) -> Result<&str, std::str::Utf8Error> {
-				let mut len = $size;
-				for (idx, &byte) in self.0.iter().enumerate() {
-					if byte == 0 {
-						len = idx;
-						break;
-					}
-				}
-				std::str::from_utf8(&self.0[..len])
-			}
-		}
-		impl std::fmt::Debug for $ident {
-			fn fmt(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-				use std::fmt::Write;
-				formatter.write_str(stringify!($ident))?;
-				formatter.write_char(' ')?;
-				self.as_str().map_err(|_| std::fmt::Error::default())?.fmt(formatter)
-			}
-		}
-		impl std::fmt::Display for $ident {
-			fn fmt(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-				self.as_str().map_err(|_| std::fmt::Error::default())?.fmt(formatter)
-			}
-		}
-		impl PartialEq for $ident {
-			fn eq(&self, other: &Self) -> bool {
-				for (&c1, &c2) in self.0.iter().zip(other.0.iter()) {
-					if c1 != c2 {
-						return false;
-					}
-					// end of string: quit early
-					// c1 == c2 due to previous block
-					if c1 == 0 {
-						return true;
-					}
-				}
-				true
-			}
-		}
-	};
+		true
+	}
 }
 
-fixed_string_type!(FileType, 4);
+pub type FileType = FixedString<4>;
 // This type is the same size as String so you might as well store it by value!
-fixed_string_type!(FileName, 24);
+pub type FileName = FixedString<24>;
 
 #[derive(Default)]
 pub struct ReadArgs {
