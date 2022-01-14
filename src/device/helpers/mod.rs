c2469e3a16818dbf1d2b83ed632cac9fead751cd
Matt Fellenz <matt@felle.nz>
2022-01-14 10:19:00 -0800

    Move version helpers into submodule

---
+++ src/device/helpers/mod.rs
@@ -0,0 +1,38 @@
+use encde::Decode;
+
+pub mod version;
+pub use version::{LongVersion, ShortVersion};
+
+#[derive(Decode)]
+pub struct BrainFlags(u8);
+impl BrainFlags {
+	// empty
+}
+
+#[derive(Decode)]
+pub struct ControllerFlags(u8);
+impl ControllerFlags {
+	pub fn connected(&self) -> bool {
+		self.0 & 0b10 == 0b10
+	}
+}
+
+#[derive(Decode)]
+#[repr(u8)]
+pub enum Product {
+	#[encde(wire_tag = 0x10)]
+	Brain(BrainFlags),
+	#[encde(wire_tag = 0x11)]
+	Controller(ControllerFlags),
+}
+
+impl std::fmt::Display for Product {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		match self {
+			Self::Brain(_) => f.write_str("brain"),
+			Self::Controller(flags) => write!(f, "controller (connected: {})", flags.connected()),
+		}
+	}
+}
+
+pub type SystemID = u32;
--- src/device/helpers/version.rs
+++ src/device/helpers/version.rs
@@ -54,37 +54,3 @@ impl std::cmp::PartialOrd<LongVersion> for ShortVersion {
 		Some(self.cmp(&other.common))
 	}
 }
-
-#[derive(Decode)]
-pub struct BrainFlags(u8);
-impl BrainFlags {
-	// empty
-}
-
-#[derive(Decode)]
-pub struct ControllerFlags(u8);
-impl ControllerFlags {
-	pub fn connected(&self) -> bool {
-		self.0 & 0b10 == 0b10
-	}
-}
-
-#[derive(Decode)]
-#[repr(u8)]
-pub enum Product {
-	#[encde(wire_tag = 0x10)]
-	Brain(BrainFlags),
-	#[encde(wire_tag = 0x11)]
-	Controller(ControllerFlags),
-}
-
-impl std::fmt::Display for Product {
-	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		match self {
-			Self::Brain(_) => f.write_str("brain"),
-			Self::Controller(flags) => write!(f, "controller (connected: {})", flags.connected()),
-		}
-	}
-}
-
-pub type SystemID = u32;
