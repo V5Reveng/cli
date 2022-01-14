833bb9e7d76df45371285bad5cf50f1314c20f8f
Matt Fellenz <matt@felle.nz>
2022-01-14 10:49:51 -0800

    Extract file diff from edit command

--- src/commands/filesystem/edit.rs
+++ src/commands/filesystem/edit.rs
@@ -1,6 +1,7 @@
 use crate::commands::Runnable;
 use crate::crc::CrcComputable;
 use crate::device::filesystem as dev_fs;
+use crate::util::diff::files_differ_and_f1_len;
 use crate::util::temp_dir::TempDir;
 use log::{error, warn};
 use std::io::{self, Read, Seek};
@@ -78,42 +79,6 @@ impl Runnable for Args {
 		}
 
 		0
-	}
-}
-
-fn files_differ_and_f1_len(f1: &mut fs::File, f2: &mut fs::File) -> std::io::Result<(bool, usize)> {
-	let f1_len = f1.metadata()?.len();
-	let f2_len = f2.metadata()?.len();
-	if f1_len != f2_len {
-		Ok((true, f1_len.try_into().unwrap()))
-	} else {
-		// files are the same size
-		const BUF_SIZE: usize = 512;
-		let mut f1_buf = [0u8; BUF_SIZE];
-		let mut f2_buf = [0u8; BUF_SIZE];
-		loop {
-			let f1_read_len = f1.read(&mut f1_buf)?;
-			let f2_read_len = f2.read(&mut f2_buf)?;
-			let read_len = match std::cmp::Ord::cmp(&f1_read_len, &f2_read_len) {
-				std::cmp::Ordering::Equal => f1_read_len,
-				std::cmp::Ordering::Less => {
-					f1.read_exact(&mut f1_buf[f1_read_len..f2_read_len])?;
-					f2_read_len
-				}
-				std::cmp::Ordering::Greater => {
-					f2.read_exact(&mut f2_buf[f2_read_len..f1_read_len])?;
-					f1_read_len
-				}
-			};
-			for i in 0..read_len {
-				if f1_buf[i] != f2_buf[i] {
-					return Ok((false, f1_len.try_into().unwrap()));
-				}
-			}
-			if read_len < BUF_SIZE {
-				return Ok((true, f1_len.try_into().unwrap()));
-			}
-		}
 	}
 }
 
