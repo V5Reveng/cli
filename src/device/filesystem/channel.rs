62585875ff18a9437a0a9b12c8cf8932dbdf45e2
Matt Fellenz <matt@felle.nz>
2022-01-16 10:56:19 -0800

    Properly name Channel variants

--- src/device/filesystem/channel.rs
+++ src/device/filesystem/channel.rs
@@ -2,12 +2,12 @@ use encde::{Decode, Encode};
 
 /// The channel that the device is functioning on.
 ///
-/// PROS switches to the download channel for ostensibly faster downloads, but presumably it restricts functionality.
+/// PROS CLI calls the file transfer channel the download channel.
 #[derive(Encode, Decode, Debug, Clone, Copy, PartialEq, Eq, Hash)]
 #[repr(u8)]
 pub enum Channel {
 	Pit = 0,
-	Download = 1,
+	FileTransfer = 1,
 }
 
 impl Default for Channel {
