09845d126d007ddf11c5e6aadb9a02df841c5f32
Matt Fellenz <matt@felle.nz>
2022-01-17 16:25:34 -0800

    program::stop: implement

--- src/commands/program/stop.rs
+++ src/commands/program/stop.rs
@@ -5,7 +5,9 @@ use crate::commands::Runnable;
 pub struct Args {}
 
 impl Runnable for Args {
-	fn run(self, _dev: crate::util::presence::Presence<crate::device::Device>) -> u32 {
-		todo!()
+	fn run(self, dev: crate::util::presence::Presence<crate::device::Device>) -> u32 {
+		let mut dev = crate::commands::unwrap_device_presence(dev);
+		dev.stop_execution().expect("Stopping execution");
+		0
 	}
 }
